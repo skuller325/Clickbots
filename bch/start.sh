257d1b5d9c160909b8a6f0c1e3d859c573f4ec10
Logicjoker{} <59412154+logicjoker@users.noreply.github.com>
2020-07-01 09:06:34 +0700

    Add files via upload

---
+++ bch/start.sh
@@ -0,0 +1,14 @@
+while true;
+do
+
+filename='telegram.txt'
+exec 4<$filename
+while read -u4 p ; do
+    python main.py -p $p -c bch
+     sleep 10
+done
+
+echo " Wait 2hours to refresh ads..."
+sleep 7200
+
+done
